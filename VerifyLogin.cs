be16ae7be80d9e99276cca690a9a9d3c1a96ff00
karlos-silva <karlossilva194@gmail.com>
2020-04-22 13:54:16 -0300

    initial commit

---
+++ VerifyLogin.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SeleniumCore
+{
+    [TestClass]
+    public class HomepageFeature
+    {
+
+        IWebDriver _driver;
+        [TestMethod]
+        public void ShouldBeAbleToLogin()
+        {
+
+            var outPutDirectory =
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _driver = new ChromeDriver(outPutDirectory);
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            var loginButtonLocator =By.ClassName("btn_action");
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(
+                ExpectedConditions.ElementIsVisible(loginButtonLocator));
+
+            var userNameField = _driver.FindElement(By.Id("user-name"));
+            var passwordField = _driver.FindElement(By.Id("password"));
+            var loginButton = _driver.FindElement(loginButtonLocator);
+
+            userNameField.SendKeys("standart_user");
+            passwordField.SendKeys("secret_sauce");
+            loginButton.Click();
+
+
+
+            Assert.IsTrue(_driver.Url.Contains("inventory.html"));
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+}
+                        
